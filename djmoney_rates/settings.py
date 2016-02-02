7f95912664f1bba6de3a31b638c42a7dac1d7ead
Antonin Lacombe <antonin@jelouemoncampingcar.com>
2016-02-02 12:07:03 +0100

    fix import_module import in settings

--- djmoney_rates/settings.py
+++ djmoney_rates/settings.py
@@ -16,7 +16,8 @@ back to the defaults.
 """
 
 from django.conf import settings
-from django.utils import importlib, six
+from importlib import import_module
+from django.utils import six
 
 
 USER_SETTINGS = getattr(settings, 'DJANGO_MONEY_RATES', None)
@@ -59,7 +60,7 @@ def import_from_string(val, setting_name):
     try:
         parts = val.split('.')
         module_path, class_name = '.'.join(parts[:-1]), parts[-1]
-        module = importlib.import_module(module_path)
+        module = import_module(module_path)
         return getattr(module, class_name)
     except ImportError as e:
         msg = "Could not import '%s' for setting '%s'. %s: %s." % (val, setting_name, e.__class__.__name__, e)
